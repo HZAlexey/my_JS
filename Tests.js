5d0b23b99775b3a9c85b4422f61888f11f4008d0
a <qa.alexey@yandex.ru>
2020-10-31 21:21:13 +0300

    first

--- Tests.js
+++ Tests.js
@@ -1,4 +1,41 @@
 
 'use strict'
-alert(1)
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("sdfdf"*3);
+
+let persone = {
+ name: "John",
+ age: 25,
+ isMarried: false
+};
+
+
+console.log(persone["name"]);
+let arr = ['plum.png', 'asdsd.jpeg', 'dsadsad.bmp'];
+
+// console.log(arr[2]);
+//
+// // let answer = confirm("Are you here?");
+// // console.log(answer)
+// //let answer = prompt("Есть ди тебе 18?", "Да");
+// console.log(typeof(null));
+let incr = 10,
+    decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log("2"===2);
+
+let isCheckit = false,
+    isClose = false;
+    console.log(isCheckit || isClose);
 
