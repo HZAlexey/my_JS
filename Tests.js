b84a500c0249d272a2e0b9715a19426615f49399
a <qa.alexey@yandex.ru>
2021-01-19 19:52:54 +0300

    commit 3

--- Tests.js
+++ Tests.js
@@ -1,6 +1,5 @@
-'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = prompt("Ваш бюджет на месяц?"),
     time = prompt("Введите дату в формате YYYY-MM-DD",'');
 
 let appData = {
@@ -12,10 +11,29 @@ let appData = {
     savings: false
 };
 
-for (let i = 0, i < 2, i++) {
+for (let i = 1; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется", '');
-    appData.expenses[a] = b;
+        b = prompt("Во сколько обойдется?", '');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null &&
+    a != '' && b != '' && a.length < 50) {
+        console.log("done")
+        appData.expenses[a] = b; // ключ : значение
+    } else {
+        // Вернуться к первому ыопросу
+    }
 }
 
-console.log(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if ( appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000 ) {
+    console.log("Высокий уровень достатка")
+} else {
+    console.log("Какая-то ошибка")
+}
---
+++ test2.js
@@ -0,0 +1,7 @@
+
+function showFirstText(text) {
+    console.log(text);
+}
+
+
+showFirstText("хули")
